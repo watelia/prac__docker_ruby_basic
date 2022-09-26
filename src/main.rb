82629517d7bd9425f501c2951262c81e64c4f821
watelia <rom.hirtsk.20@gmail.com>
2022-09-26 15:06:19 +0900

    アプリケーションの位置を変更

--- src/main.rb
+++ src/main.rb
@@ -4,11 +4,11 @@ require 'webrick'
 
 server = WEBrick::HTTPServer.new(
     DocumentRoot: './',
-    BindAddress: '0.0.0.0',
+    BindAddres: '0.0.0.0',
     Port: 8000
 )
 
-server.mount_proc('/') do |req res|
+server.mount_proc('/') do |req, res|
     res.body = 'hello'
 end
 
